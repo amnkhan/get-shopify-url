e9ba1fc079671169745918c111ea1012a632e480
amnkhan <amnkhan.me@gmail.com>
2021-08-08 06:06:53 +0600

    Deleted Old Version

--- Content.js
+++
@@ -1,8 +0,0 @@
-if (window.Shopify) {
-  const shopifyURL = Shopify.shop;
-  const shopifyTheme = Shopify.theme;
-  console.log(shopifyURL);
-  console.log(shopifyTheme.name);
-} else {
-  console.log("This is not a Shopify store");
-}
